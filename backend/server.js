973eed8df457d6d5aa54ab3ab615f0a97207e02a
esvance77inwza <your@email.com>
2026-03-09 16:11:45 +0700

    Update DB Connection

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,11 @@ app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // เชื่อมต่อ MongoDB (ต้องเปิด MongoDB หรือ xampp/docker ที่มี MongoDB รันอยู่ด้วยนะครับ)
+// เปลี่ยนโค้ดเป็นแบบนี้ครับ
 mongoose
-  .connect('mongodb://127.0.0.1:27017/my_shop_db')
-  .then(() => console.log('✅ Connected to MongoDB successfully!'))
-  .catch((err) => console.error('❌ MongoDB connection error:', err))
+  .connect(process.env.MONGO_URI || 'mongodb://localhost:27017/testshop')
+  .then(() => console.log('✅ Connected to Database'))
+  .catch((err) => console.log(err))
 
 // Routes
 app.use('/api/products', require('./routes/products'))
